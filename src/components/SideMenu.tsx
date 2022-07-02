bf0ff2402109ecd35c217346fa7ee59ace2bf6cc
LGWayneq <g_wayneliau@yahoo.com.sg>
2022-07-02 23:48:27 +0800

    add some stylings

--- src/components/SideMenu.tsx
+++ src/components/SideMenu.tsx
@@ -2,10 +2,16 @@ import React from 'react';
 import { colours } from '../assets/colours';
 import WindowDimensions from '../utils/WindowDimensions';
 import { SIDE_MENU_WIDTH, TITLE_BAR_HEIGHT } from '../assets/constants';
+import PeopleIcon from '@mui/icons-material/People';
+import KeyboardIcon from '@mui/icons-material/Keyboard';
+import SettingsIcon from '@mui/icons-material/Settings';
 
 function SideMenu() {
     return (
         <div style={{ ...styles.container, height: WindowDimensions().height - TITLE_BAR_HEIGHT - 2 * padding }}>
+            <PeopleIcon sx={styles.icon} onClick={() => {}}/>
+            <KeyboardIcon sx={styles.icon} onClick={() => {}}/>
+            <SettingsIcon sx={styles.icon} onClick={() => {}}/>
         </div>
     );
 }
@@ -17,9 +23,16 @@ const padding = 5
 const styles = {
     container: {
         display: 'flex',
+        flexDirection: 'column' as 'column',
         backgroundColor: colours.menu,
         width: SIDE_MENU_WIDTH,
-        justifyContent: 'center',
+        justifyContent: 'flex-start',
+        alignItems: 'center',
         padding: padding
+    },
+    icon: {
+        color: 'white',
+        margin: 1,
+        cursor: 'pointer'
     }
 }
