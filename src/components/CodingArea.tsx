b3d2da81c9c3693faa8da4e08573bb8dc4d7c2d2
LGWayneq <g_wayneliau@yahoo.com.sg>
2022-07-04 17:14:43 +0800

    implement feature to prevent press and hold typing

--- src/components/CodingArea.tsx
+++ src/components/CodingArea.tsx
@@ -18,32 +18,44 @@ function CodingArea() {
     const [codeLines, setCodeLines] = useState<Array<JSX.Element>>(initialCodeLines)
     const [currentIndex, setCurrentIndex] = useState<number>(1)
     const [currentLine, setCurrentLine] = useState<number>(0)
+    const [keypressed, setKeypressed] = useState<boolean>(false)
     const cpk = useAppSelector(state => state.cpk.value)   // cpk - characters per press
     const cpms = useAppSelector(state => state.cpms.value)
     const dispatch = useAppDispatch()
 
     useEffect(() => {
-        //should combine with cpms logic
-        function handleKeyPress(this: Window, event: KeyboardEvent) {
-            updateCodeLines(currentIndex, cpk)
-            trimCodeLines()
+        function handleKeyDown() {
+            console.log(keypressed)
+
+            if (!keypressed) {
+                updateCodeLines(currentIndex, cpk)
+                trimCodeLines()
+            }
+            setKeypressed(true)
         }
-        window.addEventListener('keypress', handleKeyPress)
-        return () => window.removeEventListener('keypress', handleKeyPress)
-    }, [currentIndex])
+        function handleKeyUp() {
+            setKeypressed(false)
+        }
+        window.addEventListener('keydown', handleKeyDown)
+        window.addEventListener('keyup', handleKeyUp)
+        return () => {
+            window.removeEventListener('keydown', handleKeyDown)
+            window.removeEventListener('keyup', handleKeyUp)
+        }
+    }, [currentIndex, keypressed])
 
     useEffect(() => {
-        //temporary use setInterval. should change to setTimeout to refetch cpms
+        //todo: add handling of cpms and cpk float values
         const idleUpdater = setInterval(() => {
-            dispatch(incrementByAmount(cpms))
+            updateCodeLines(currentIndex, cpms)
         }, 100)
         return () => clearInterval(idleUpdater)
     })
 
-    const updateCodeLines = (currentIndex: number, cpk: number) => {
+    const updateCodeLines = (currentIndex: number, increment: number) => {
         var _currentLine: number = currentLine
         var currentCodeLine: JSX.Element | undefined
-        for (var i = 0; i < cpk; i++) {
+        for (var i = 0; i < increment; i++) {
             //check if reached end of codeContent. loop back to first index
             if (currentIndex + i >= codeContent.length) {
                 currentIndex = -i + 1
@@ -68,7 +80,7 @@ function CodingArea() {
         if (codeContent[currentIndex + i - 1] != '\n') {
             appendCodeLine(currentCodeLine)
         }
-        setCurrentIndex(currentIndex + cpk)
+        setCurrentIndex(currentIndex + increment)
     }
 
     const appendCodeLine = (newCodeLine: JSX.Element | undefined) => {
